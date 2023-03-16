6b6f8d1d78a28a9778900dd325e979bb447f66d8
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-16 19:44:11 -0300

    :sparkles: Implement custom error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,3 +1,4 @@
+import { UserAlreadyExistsError } from './../../errors/user-already-exists-error'
 import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
 import { RegisterService } from '@/services/register'
 import { FastifyRequest, FastifyReply } from 'fastify'
@@ -17,7 +18,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerService = new RegisterService(repository)
     await registerService.execute({ name, email, password })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
