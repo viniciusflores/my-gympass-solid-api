d75b6c9b7908ef709c8b21db6dbfbc84a960464c
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-18 17:21:54 -0300

    :contruction: WIP checkin service - fetch history

---
+++ src/services/fetch-user-check-ins-history.ts
@@ -0,0 +1,27 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface IFetchUserCheckInsHistoryResquest {
+  userId: string
+  page: number
+}
+
+interface IFetchUserCheckInsHistoryRespose {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistory {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: IFetchUserCheckInsHistoryResquest): Promise<IFetchUserCheckInsHistoryRespose> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return { checkIns }
+  }
+}
