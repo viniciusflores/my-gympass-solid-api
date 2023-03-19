e44cacb6259490e85f522b63677702769f017982
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-19 13:10:54 -0300

    :sparkles: Create search gym service

---
+++ src/services/searchGym.spec.ts
@@ -0,0 +1,62 @@
+import { SearchGymService } from './searchGym'
+import { GymsRepositoryMock } from '@/repositories/mock/gym-repository-mock'
+import { describe, it, expect, beforeEach } from 'vitest'
+
+let gymsRepository: GymsRepositoryMock
+let sut: SearchGymService
+
+describe('Search Gym Service', () => {
+  beforeEach(async () => {
+    gymsRepository = new GymsRepositoryMock()
+    sut = new SearchGymService(gymsRepository)
+  })
+
+  it('should be able to fetch check in history', async () => {
+    await gymsRepository.create({
+      title: 'JS Gym',
+      description: '',
+      phone: '',
+      latitude: -29.6821417,
+      longitude: -51.1279104,
+    })
+
+    await gymsRepository.create({
+      title: 'TS Gym',
+      description: '',
+      phone: '',
+      latitude: -29.6821417,
+      longitude: -51.1279104,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'JS',
+      page: 1,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'JS Gym' })])
+  })
+
+  it('should be able to fetch paginated check in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `JS Gym : ${i}`,
+        description: '',
+        phone: '',
+        latitude: -29.6821417,
+        longitude: -51.1279104,
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: 'JS',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'JS Gym : 21' }),
+      expect.objectContaining({ title: 'JS Gym : 22' }),
+    ])
+  })
+})
---
+++ src/services/searchGym.ts
@@ -0,0 +1,24 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from './../repositories/gyms-repository'
+
+interface IsearchGymServiceRequest {
+  query: string
+  page: number
+}
+
+interface ISerachGymResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymService {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: IsearchGymServiceRequest): Promise<ISerachGymResponse> {
+    const gyms = await this.gymsRepository.searchMany(query, page)
+
+    return { gyms }
+  }
+}
