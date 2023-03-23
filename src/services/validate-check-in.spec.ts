e1ac0067e3897ded097aa28536f7f754fe2245e6
Vinicius Flores <viniciusflores379@gmail.com>
2023-03-23 17:30:09 -0300

    :sparkles: implement validate checkin in 20 minutes

--- src/services/validate-check-in.spec.ts
+++ src/services/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
 import { ValidateCheckInService } from './validate-check-in'
 import { CheckInRepositoryMock } from '@/repositories/mock/check-in-repository-mock'
 import { ResourceNotFoundError } from './errors/resource-not-found'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 let checkInsRepository: CheckInRepositoryMock
 let sut: ValidateCheckInService
@@ -10,6 +11,12 @@ describe('Validate Check In Service', () => {
   beforeEach(async () => {
     checkInsRepository = new CheckInRepositoryMock()
     sut = new ValidateCheckInService(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -25,10 +32,29 @@ describe('Validate Check In Service', () => {
   })
 
   it('should not be able to validate an inexistance check-in', async () => {
-    expect(() =>
+    await expect(() =>
       sut.execute({
         checkInId: 'inexistent-check-in-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate a check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const checkIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(() =>
+      sut.execute({
+        checkInId: checkIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
