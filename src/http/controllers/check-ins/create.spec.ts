23e65d210fdb9b117967acca44665a4603f278cb
Vinicius Flores <viniciusflores379@gmail.com>
2023-04-17 07:05:02 -0300

    :sparkles: Implement check-ins e2e tests

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/tests/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JS Gym',
+        latitude: -29.6821417,
+        longitude: -51.1279104,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -29.6821417,
+        longitude: -51.1279104,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
