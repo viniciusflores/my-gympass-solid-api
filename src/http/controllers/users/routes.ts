a4d60f654753719e41f92bbaecabb3b2fb463a5b
Vinicius Flores <viniciusflores379@gmail.com>
2023-04-17 08:48:06 -0300

    :construction: create controller to refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 import { register } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /* Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
